ce885164d6186f7171df8c2690fb1bbed34a768b
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-04 13:07:22 +0000

    autoupdate 2013-11-04-13:07:22

--- python/testread.py
+++ python/testread.py
@@ -8,6 +8,6 @@ plot.readconfig(configfile)
 
 for i in range(1,101):
     print i
-    linefile = '/scratch/01937/cs390/outputs/cubepm_090610_14_5488_20Mpc/RESULT/ext1mpc/8.064/RG/0.000/0/sout.%d' % (i)
+    linefile = '/scratch/01937/cs390/outputs/cubepm_090610_14_5488_20Mpc/RESULT/ext1mpc/8.064/RG/0.200/0/sout.%d' % (i)
     line = plot.readline_binary(linefile)
     plot.PrepFFT(line[:,3],line[:,4],line[:,5])
