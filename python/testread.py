3b2bcd9f64cb566cc89b4749820d371c24b540b3
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-04 12:55:35 +0000

    autoupdate 2013-11-04-12:55:35

--- python/testread.py
+++ python/testread.py
@@ -10,9 +10,4 @@ for i in range(1,101):
     print i
     linefile = '/scratch/01937/cs390/outputs/cubepm_090610_14_5488_20Mpc/RESULT/ext1mpc/8.064/RG/0.200/0/sout.%d' % (i)
     line = plot.readline_binary(linefile)
-    for halo in line:
-        dist = plot.nu2Mpc(halo[2])
-        undist = plot.nu2Mpc(halo[3])
-        print dist,undist
-
-#plot.plotline(line[:,3],line[:,4],line[:,5],'0.000.pdf')
+    plot.PrepFFT(line[:,3],line[:,4],line[:,5])
