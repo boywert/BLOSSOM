f802d7da9d8ad07bca24b21b7a8b023b20f57eeb
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-04 16:35:51 +0000

    autoupdate 2013-11-04-16:35:51

--- python/plot.py
+++ python/plot.py
@@ -43,7 +43,7 @@ def plotline(freq,absorp,width,output):
     yarray = numpy.ones(len(xarray))
     for i in range(len(freq)):
         realwidth = width[i]/parameters.config['nu0']*freq[i]
-        yarray *= (1.-absorp[i])/numpy.sqrt(2.*numpy.pi)/realwidth*numpy.exp(-0.5*((nuarray-freq[i])/realwidth)**2)
+        yarray *= (1.-absorp[i])/numpy.sqrt(2.*numpy.pi)/realwidth*numpy.exp(-0.5*((xarray-freq[i])/realwidth)**2)
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.plot(xarray,yarray)
