e316330a024befdd0a467a04fa172a8c35576afd
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-04 12:00:44 +0000

    autoupdate 2013-11-04-12:00:44

--- python/testread.py
+++ python/testread.py
@@ -6,13 +6,12 @@ configfile = '/home1/01937/cs390/BLOSSOM/inputs/config_20mpc_z8_ext1mpc'
 plot.readconfig(configfile)
 
 
-for i in range(1,100):
+for i in range(1,101):
     print i
     linefile = '/scratch/01937/cs390/outputs/cubepm_090610_14_5488_20Mpc/RESULT/ext1mpc/8.064/RG/0.000/0/sout.%d' % (i)
     line = plot.readline_binary(linefile)
     for eachline in line:
         for lineel in eachline:
-            if(lineel < 0.):
-                print eachline
+            print eachline
 
 #plot.plotline(line[:,3],line[:,4],line[:,5],'0.000.pdf')
