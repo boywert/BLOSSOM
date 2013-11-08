4246f9d81f588aa88bf85b12ef8aa47af8e35e0f
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-08 16:43:41 +0000

    autoupdate 2013-11-08-16:43:41

---
+++ src/module_subcell_cor.f90
@@ -0,0 +1,5 @@
+module mod_subcell_corr
+  implicit none
+contains
+#include "subcell_corr.f90"
+end module mod_subcell_corr
--- src/runprocs.f90
+++ src/runprocs.f90
@@ -1,18 +1,15 @@
 module runprocs
+  use mod_subcell_corr
   implicit none
 contains
 
 !#include "massfunction.f90"
-#include "subcell_corr.f90"
 
 #include "gen_los.f90"
-!#include "makecorrelation.f90"
-!#include "absorpstats.f90"
 #include "makeobservedline.f90"
 
 #define RR
 #include "gen_los.f90"
-!#include "makecorrelation.f90"
 #include "makeobservedline.f90"
 #include "gen_random.f90"
 
