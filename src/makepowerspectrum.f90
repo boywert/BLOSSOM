fe8964c39d3f8fc2cc59622c8f23aa1d52571154
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-09 13:13:29 +0000

    autoupdate 2013-11-09-13:13:29

--- src/makepowerspectrum.f90
+++ src/makepowerspectrum.f90
@@ -11,6 +11,7 @@ subroutine makepowerspectrum_rg(z)
   use, intrinsic :: iso_c_binding
   implicit none
   include 'fftw3.f'
+  real(kind=8) :: z
   integer, parameter :: N=10
   integer(kind=8) :: plan
   complex(kind=8) :: in(N)
