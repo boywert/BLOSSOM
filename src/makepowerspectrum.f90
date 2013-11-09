3e0bb39a53b5f4f17f96ccbaf56c85000b506418
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-09 20:30:03 +0000

    autoupdate 2013-11-09-20:30:03

--- src/makepowerspectrum.f90
+++ src/makepowerspectrum.f90
@@ -22,7 +22,7 @@ subroutine makepowerspectrum_rg(z)
   end do
   print*,""
   !$omp parallel do
-  do i=0,100
+  do i=0,10000
      call dfftw_plan_dft_r2c_1d(plan(omp_get_thread_num()),N,in(:,omp_get_thread_num()),out(:,omp_get_thread_num()),FFTW_ESTIMATE)
      call dfftw_execute(plan(omp_get_thread_num()))
      call dfftw_destroy_plan(plan(omp_get_thread_num()))
