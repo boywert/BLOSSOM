a2c9fd30e200f2b028c9a6b7426470a0e74cb79f
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-10 16:10:06 +0000

    autoupdate 2013-11-10-16:10:06

--- src/makepowerspectrum.f90
+++ src/makepowerspectrum.f90
@@ -6,33 +6,49 @@ subroutine makepowerspectrum_rg(z)
   use mpi
   use mpitools
   use common_vars
+  use conversiontools
   use omp_lib
   use io_tools
   implicit none
   include 'fftw3.f'
   real(kind=8) :: z
   integer, parameter :: N=1000
-  integer(kind=8) :: plan(0:2)
-  real(kind=8) :: in(N,0:2)
-  complex(kind=8) :: out(N/2+1,0:2)
+  integer(kind=8),allocatable :: plan(:)
+  real(kind=8),allocatable :: in(:,:)
+  complex(kind=8),allocatable :: out(:,:)
   integer :: i,j,iret
-
-  do i=1,N
-     in(i,0:2) = real(i)**2.
+  real(kind=8) :: nu_max,nu_min,max_observe,min_observe
+  integer :: freq_nbins
+  real(kind=8), allocatable :: frequency_value(:),tmp_distance_value(:)
+  !Calculate frequency and distance range
+  d0 = z_to_d(z)
+  max_observe = d0 + convert_length2physical(real(Boxsize*line_length_factor/2.,8),z) 
+  nu_min = d_to_nu(max_observe)
+  min_observe = d0 - convert_length2physical(real(Boxsize*line_length_factor/2.,8),z) 
+  nu_max = d_to_nu(min_observe)
+  freq_nbins = ceiling((nu_max-nu_min)/maxfreqresolution)
+  
+  !Allocate arrays
+  allocate(frequency_value(0:freq_nbins))
+  allocate(tmp_distance_value(0:freq_nbins))
+  do i=0,freq_nbins
+     frequency_value(i) = max_nu - i*maxfreqresolution
+     tmp_distance_value(i) = nu_to_d(frequency_value(i))
   end do
-  print*,""
-  !$omp parallel do
-  do i=0,10000
-     call dfftw_plan_dft_r2c_1d(plan(omp_get_thread_num()),N,in(:,omp_get_thread_num()),out(:,omp_get_thread_num()),FFTW_ESTIMATE)
-     call dfftw_execute(plan(omp_get_thread_num()))
-     call dfftw_destroy_plan(plan(omp_get_thread_num()))
+  do i=freq_nbins,1,-1
+     tmp_distance_value(i) = (tmp_distance_value(i)-tmp_distance_value(i-1))/Mpc
   end do
-  !$omp end parallel do
-  ! do i=1,N/2+1
-  !    print*, real(in(i)), real(out(i))
+  tmp_distance_value(0) = 0.
+  do i=0,freq_nbins
+     print*,tmp_distance_value(i)
+  end do
+  ! do j= first_l,last_l
+  !    call dfftw_plan_dft_r2c_1d(plan(omp_get_thread_num()),N,in(:,omp_get_thread_num()),out(:,omp_get_thread_num()),FFTW_ESTIMATE)
+  !    call dfftw_execute(plan(omp_get_thread_num()))
+  !    call dfftw_destroy_plan(plan(omp_get_thread_num()))
   ! end do
-
-
+     
+  deallocate(frequency_value,tmp_distance_value)
   
 #ifdef RR
 end subroutine makepowerspectrum_rr
