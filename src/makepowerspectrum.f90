96b192022d48b528747c2c24149f1ea00920047a
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-10 16:35:01 +0000

    autoupdate 2013-11-10-16:35:01

--- src/makepowerspectrum.f90
+++ src/makepowerspectrum.f90
@@ -36,10 +36,9 @@ subroutine makepowerspectrum_rg(z)
   do i=0,freq_nbins
      frequency_value(i) = nu_max - i*obsfreqresolution
      tmp_distance_value(i) = nu_to_d(frequency_value(i))
-     print*,tmp_distance_value(i)/kpc
   end do
   do i=freq_nbins,1,-1
-     tmp_distance_value(i) = (tmp_distance_value(i)-tmp_distance_value(i-1))/kpc
+     tmp_distance_value(i) = (tmp_distance_value(i)-tmp_distance_value(0))/kpc
   end do
   tmp_distance_value(0) = 0.
   do i=0,freq_nbins
