9e1f5ccdf8859dac760f52bc3eac1e148d7245e1
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-11 17:26:13 +0000

    autoupdate 2013-11-11-17:26:13

--- src/makepowerspectrum.f90
+++ src/makepowerspectrum.f90
@@ -92,13 +92,11 @@ subroutine makepowerspectrum_rg(z)
  
      
      call array_intrpol(tmp_distance_value(0:obs_freq_nbins-1),tmp_signal_obs(0:obs_freq_nbins-1),obs_freq_nbins,x_array(0:x_nbins-1),y_array(0:x_nbins-1),x_nbins)
-     do i=0,x_nbins-1
-         y_array(i) = exp(-1.*(real(i,8)))
-      end do
-     ! y_array = 1. - y_array
-     ! mean_den = sum(y_array)/x_nbins
-     ! y_array = y_array/mean_den -1.
-     ! y_array
+
+     y_array = 1. - y_array
+     mean_den = sum(y_array)/x_nbins
+     y_array = y_array/mean_den -1.
+
      call dfftw_plan_dft_r2c_1d(plan,x_nbins,y_array,fft_result,FFTW_ESTIMATE)
      call dfftw_execute_dft_r2c(plan,y_array,fft_result)
 
