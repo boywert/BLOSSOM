30e8b5e058dd5ec56006f00ec35637d7cb2da515
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-11 14:53:52 +0000

    autoupdate 2013-11-11-14:53:52

--- src/makepowerspectrum.f90
+++ src/makepowerspectrum.f90
@@ -96,8 +96,8 @@ subroutine makepowerspectrum_rg(z)
      !    y_array(i) = real(i,8)
      ! end do
      y_array = 1. - y_array
-     mean_den = sum(y_array)
-     y_array(0:x_nbins-1) = y_array(0:x_nbins-1)/mean_den -1.
+     mean_den = sum(y_array)/x_nbins
+     y_array = y_array/mean_den -1.
      call dfftw_plan_dft_r2c_1d(plan,x_nbins,y_array,fft_result,FFTW_ESTIMATE)
      call dfftw_execute(plan)
 
