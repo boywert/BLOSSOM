e91ce79bd596e9275f2472ffa6941fa99d746f55
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-12 23:37:38 +0000

    autoupdate 2013-11-12-23:37:38

--- src/makepowerspectrum.f90
+++ src/makepowerspectrum.f90
@@ -73,7 +73,7 @@ subroutine makepowerspectrum_rg(z)
      write(str_line,'(i10)') j
      str_line = adjustl(str_line)
 
-     tmp_signal_fine(:) = 0.0
+     tmp_signal_fine(:) = 1.0
      tmp_signal_obs(:) = 0.0
      open (unit=10, &
           file=trim(result_path)//z_s(1:len_trim(z_s))//'/RG/0.000/'//trim(adjustl(str_rank))//'/sout.'//trim(adjustl(str_line)), &
@@ -94,16 +94,16 @@ subroutine makepowerspectrum_rg(z)
      
      call array_intrpol(tmp_distance_value(0:obs_freq_nbins-1),tmp_signal_obs(0:obs_freq_nbins-1),obs_freq_nbins,x_array(0:x_nbins-1),y_array(0:x_nbins-1),x_nbins)
 
-     ! y_array = 1./y_array - 1.
-     ! mean_den = sum(y_array)/x_nbins
-     ! y_array = y_array/mean_den -1.
+     y_array = 1./y_array - 1.
+     mean_den = sum(y_array)/x_nbins
+     y_array = y_array/mean_den -1.
      ! y_array(:) = 0.
      ! do i=0,x_nbins-1
      !    if(mod(i,10) == 0) y_array(i) = y_array(i) + 50.
      !    if(mod(i,3) == 0) y_array(i) = y_array(i) + 100.
      ! end do
-     mean_den = sum(y_array)/x_nbins
-     y_array = y_array/mean_den -1.
+     ! mean_den = sum(y_array)/x_nbins
+     ! y_array = y_array/mean_den -1.
      call dfftw_plan_dft_r2c_1d(plan,x_nbins,y_array,fft_result,FFTW_ESTIMATE)
      call dfftw_execute_dft_r2c(plan,y_array,fft_result)
 
