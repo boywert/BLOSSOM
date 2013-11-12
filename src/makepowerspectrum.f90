3226f139e0b6c5008ae8e62c9bbe3d534b8bb132
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-12 00:57:24 +0000

    autoupdate 2013-11-12-00:57:24

--- src/makepowerspectrum.f90
+++ src/makepowerspectrum.f90
@@ -118,7 +118,7 @@ subroutine makepowerspectrum_rg(z)
   end do
   do i=1,x_nbins-1
      ps_3d(i) = -1.* (ps_1D(i)-ps_1D(i-1))/(k_1D(i)-k_1D(i-1))*2.*pi/(k_1D(i))
-     print*,k_1D(i),ps_3d(i)
+     print*,k_1D(i),ps_3d(i),ps_1D(i)
   end do
   call exit
      
