cde6a9d17297c3af09a7d09473948f3d3ace401f
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2013-11-12 23:55:35 +0000

    autoupdate 2013-11-12-23:55:35

--- src/makepowerspectrum.f90
+++ src/makepowerspectrum.f90
@@ -93,7 +93,7 @@ subroutine makepowerspectrum_rg(z)
      
      call array_intrpol(tmp_distance_value(0:obs_freq_nbins-1),tmp_signal_obs(0:obs_freq_nbins-1),obs_freq_nbins,x_array(0:x_nbins-1),y_array(0:x_nbins-1),x_nbins)
 
-     y_array = 1./y_array - 1.
+     y_array = 1.- y_array 
      mean_den = sum(y_array)/x_nbins
      y_array = y_array/mean_den -1.
 
